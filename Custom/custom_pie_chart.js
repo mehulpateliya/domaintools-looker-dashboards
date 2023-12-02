3a90a2495f1f1131b5a210467c1004c7d0e1efc7
Sagar Malaviya <sagar.malaviya@crestdatasys.com>
2023-12-02 09:27:40 +0000

    DTGC-181 update condition

--- Custom/custom_pie_chart.js
+++ Custom/custom_pie_chart.js
@@ -87,17 +87,17 @@
         datasets: [{ data: FinalData, backgroundColor: ["#1A73E8", "#12B5CB", "#E52592", "#E8710A", "#F9AB00", "#7CB342", "#9334E6", "#80868B", "#079c98", "#A8A116", "#EA4335", "#FF8168", "#76abf1", "#71d3e0", "#ef7cbe", "#f1aa6c", "#fbcd66", "#b0d18e", "#be85f0", "#b3b6b9"] }],
         labels: finalLabel
       }
-      else {
+      // Update the chart with the data
+      this.chart.data = finalCharData;
+      this.chart.update();
+    }
+    else {
         var canvas = document.getElementById("customPieChartCanvas")
         canvas.innerHTML = `<div style="align-items: center;">
                     <h2>No Data Available</h2>
                     </div>
                   `;
       }
-      // Update the chart with the data
-      this.chart.data = finalCharData;
-      this.chart.update();
-    }
       // Signal the completion of rendering
       doneRendering();
     },
